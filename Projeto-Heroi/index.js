37ec19745afad847d61814bf1b43d6a8263c0dae
Bruno Mabreu <bruno.mabreu01@gmail.com>
2025-11-19 18:22:41 -0300

    atualização

--- Projeto-Heroi/index.js
+++ Projeto-Heroi/index.js
@@ -1,25 +1,28 @@
-let heroi = prompt("Qual o nome do herói?");
-let XP = parseInt(prompt("Quantos pontos de experiência (XP) o herói possui?"));
+const readline = require('readline-sync');
+
+let heroi = readline.question("Qual o nome do herói? ");
+let XP = parseInt(readline.question("Quantos pontos de experiência (XP) o herói possui? "));
+
 let nivel = "";
 
 if (XP < 1000) {
     nivel = "Ferro";
 } else if (XP < 2000) {
     nivel = "Bronze";
-} else if (XP < 5000) {
+} else if (XP < 3000) {
     nivel = "Prata";
-} else if (XP < 7000) {
+} else if (XP < 4000) {
     nivel = "Ouro";
-} else if (XP < 8000) {
+} else if (XP < 5000) {
     nivel = "Platina";
-} else if (XP < 9000) {
+} else if (XP < 6000) {
+    nivel = "Diamante";
+} else if (XP < 7000) {
     nivel = "Ascendente";
-} else if (XP < 10000) {
+} else if (XP < 8000) {
     nivel = "Imortal";
 } else {
     nivel = "Radiante";
 }
 
-console.log("O herói de nome " + heroi + " está no nível " + nivel);
-
-
+console.log(`O herói ${heroi} está no nível ${nivel}.`);
